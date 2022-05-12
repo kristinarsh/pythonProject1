56d25495014bdfe4c3e2ebe6f86e2e3ded5bc3be
kristinarsh <testuserbpm@mail.ru>
2022-05-12 13:09:45 +0700

    Первичный графический интерфейс

--- 4lb.py
+++ 4lb.py
@@ -1,3 +1,11 @@
 import tkinter
 from tkinter import *
 import re
+
+# Объект окна верхнего уровня создается от класса Tk
+# модуля tkinter. Переменную, связываемую с объектом,
+# часто называют root (корень):
+root = tkinter.Tk()
+root.title("Текстовый редактор")
+
+root.mainloop()
