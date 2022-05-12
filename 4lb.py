8fef0bee710651cf966549ba2a23086329ee4039
kristinarsh <testuserbpm@mail.ru>
2022-05-12 13:33:59 +0700

    Готовая программа с регулярным выражением

--- 4lb.py
+++ 4lb.py
@@ -2,6 +2,23 @@ import tkinter
 from tkinter import *
 import re
 
+
+
+def cct(event):
+    a = text.get(1.0, END)
+    text.delete(1.0, END)
+    text.tag_config('tag', foreground='red')
+    text.tag_config('content', foreground='blue')
+    fr = 0
+    if list(re.finditer("<[^<>]+>", a))!= []:
+        for m in re.finditer("<[^<>]+>", a):
+            text.insert(INSERT, a[fr:m.start()-1], 'content')
+            text.insert(INSERT, a[m.start():m.end()], 'tag')
+            text.insert(INSERT, a[m.end() + 1:], 'content')
+            fr = m.end()
+    else:
+        text.insert(END, a[:-1], 'content')
+
 # Объект окна верхнего уровня создается от класса Tk
 # модуля tkinter. Переменную, связываемую с объектом,
 # часто называют root (корень):
@@ -18,4 +35,6 @@ scrollb.pack(side="right", fill="y")
 text.configure(yscrollcommand=scrollb.set)
 
 text.pack()
+text.bind('<Key>', cct)
+
 root.mainloop()
