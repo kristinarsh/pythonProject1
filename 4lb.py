0b044abf8c2b7d9b5859e8a8f744329c845397ed
kristinarsh <testuserbpm@mail.ru>
2022-05-12 13:27:43 +0700

    Сделали окно для ввода текста со скролл баром

--- 4lb.py
+++ 4lb.py
@@ -7,5 +7,15 @@ import re
 # часто называют root (корень):
 root = tkinter.Tk()
 root.title("Текстовый редактор")
+# Размеры окна
+root.minsize(width=500, height=500)
+root.maxsize(width=500, height=500)
+# создаем текстовое поле
+text = tkinter.Text(root, width=400, height=400, wrap="word")
+# Настройка скролл бара
+scrollb = Scrollbar(root, orient=VERTICAL, command=text.yview)
+scrollb.pack(side="right", fill="y")
+text.configure(yscrollcommand=scrollb.set)
 
+text.pack()
 root.mainloop()
